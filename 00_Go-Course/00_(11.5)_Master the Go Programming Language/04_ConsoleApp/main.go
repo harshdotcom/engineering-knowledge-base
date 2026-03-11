046370a55e49a7396b50a6cc12328fc83e4ffb73
Harsh Jha <harshj92002@gmail.com>
2026-03-11 18:58:55 +0530

    Added main.go

--- 00_Go-Course/00_(11.5)_Master the Go Programming Language/04_ConsoleApp/main.go
+++ 00_Go-Course/00_(11.5)_Master the Go Programming Language/04_ConsoleApp/main.go
@@ -22,5 +22,24 @@ func main() {
 		_ = keyboard.Close()
 	}()
 
-	fmt.Println("Press Any Key on the keyboard")
+	fmt.Println("Press Any Key on the keyboard. Press ESC to quit")
+
+	for {
+		char, key, err := keyboard.GetSingleKey()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if key != 0 {
+			fmt.Println("You Pressed", char, key)
+		} else {
+			fmt.Println("You Pressed", char, key)
+		}
+
+		if key == keyboard.KeyEsc {
+			break
+		}
+
+		fmt.Println("Program existing")
+	}
 }
