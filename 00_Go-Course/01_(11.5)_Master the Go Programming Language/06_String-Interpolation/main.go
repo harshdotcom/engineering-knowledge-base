2368e658fb350955d306b5ab8da0062fb233c0a6
Harsh Jha <harshj92002@gmail.com>
2026-04-05 22:20:39 +0530

    Completed section 3

--- 00_Go-Course/01_(11.5)_Master the Go Programming Language/06_String-Interpolation/main.go
+++ 00_Go-Course/01_(11.5)_Master the Go Programming Language/06_String-Interpolation/main.go
@@ -3,19 +3,34 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"strings"
+
+	"github.com/eiannone/keyboard"
 )
 
 var reader *bufio.Reader
 
+type User struct {
+	UserName       string
+	Age            int
+	FavoriteNumber float64
+	OwnsADog       bool
+}
+
 func main() {
 	reader = bufio.NewReader(os.Stdin)
-	userName := readString("What is your name")
-	age := readInt("How old are you")
 
-	fmt.Println("Your name is", userName, "and you are", age, "years old")
+	var user User
+
+	user.UserName = readString("What is your name")
+	user.Age = readInt("How old are you")
+	user.FavoriteNumber = readFloat("What is your favorite Number")
+	user.OwnsADog = readBool("Do you owns a dog(y/n)?")
+
+	fmt.Println("Your name is", user.UserName, "and you are", user.Age, "years old", "and your favorite number is", user.FavoriteNumber, "Owns a dog", user.OwnsADog)
 
 }
 
@@ -60,4 +75,52 @@ func readInt(s string) int {
 
 	}
 
+}
+
+func readFloat(s string) float64 {
+
+	for {
+
+		fmt.Println(s)
+		prompt()
+		userInput, _ := reader.ReadString('\n')
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		userInput = strings.Replace(userInput, "\n", "", -1)
+
+		num, err := strconv.ParseFloat(userInput, 64)
+		if err != nil {
+			fmt.Println("Please enter a number")
+		} else {
+			return num
+		}
+
+	}
+
+}
+
+func readBool(s string) bool {
+	err := keyboard.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		_ = keyboard.Close()
+	}()
+
+	for {
+		fmt.Println(s)
+		char, _, err := keyboard.GetSingleKey()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if strings.ToLower(string(char)) != "y" && strings.ToLower(string(char)) != "n" {
+			fmt.Println("Please type y or n")
+		} else if char == 'n' || char == 'N' {
+			return false
+		} else if char == 'y' || char == 'Y' {
+			return true
+		}
+	}
 }
