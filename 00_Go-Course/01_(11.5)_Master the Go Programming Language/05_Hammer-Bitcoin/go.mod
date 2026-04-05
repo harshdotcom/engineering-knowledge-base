efef10f1c88fa36c57b50460fad6ff59da4dffb1
Harsh Jha <harshj92002@gmail.com>
2026-04-05 21:47:09 +0530

    learnt string interpolation

---
+++ 00_Go-Course/01_(11.5)_Master the Go Programming Language/05_Hammer-Bitcoin/go.mod
@@ -0,0 +1,15 @@
+module myApp
+
+go 1.25.5
+
+require github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203
+
+require (
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+)
+
+require (
+	github.com/fatih/color v1.19.0
+	golang.org/x/sys v0.42.0 // indirect
+)
---
+++ 00_Go-Course/01_(11.5)_Master the Go Programming Language/06_String-Interpolation/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var reader *bufio.Reader
+
+func main() {
+	reader = bufio.NewReader(os.Stdin)
+	userName := readString("What is your name")
+	age := readInt("How old are you")
+
+	fmt.Println("Your name is", userName, "and you are", age, "years old")
+
+}
+
+func prompt() {
+	fmt.Println("-> ")
+}
+
+func readString(s string) string {
+	for {
+		fmt.Println(s)
+		prompt()
+		userInput, _ := reader.ReadString('\n')
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		userInput = strings.Replace(userInput, "\n", "", -1)
+
+		if userInput == "" {
+			fmt.Println("Please enter a valid name")
+		} else {
+			return userInput
+		}
+
+	}
+
+}
+
+func readInt(s string) int {
+
+	for {
+
+		fmt.Println(s)
+		prompt()
+		userInput, _ := reader.ReadString('\n')
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		userInput = strings.Replace(userInput, "\n", "", -1)
+
+		num, err := strconv.Atoi(userInput)
+		if err != nil {
+			fmt.Println("Please enter a whole number")
+		} else {
+			return num
+		}
+
+	}
+
+}
